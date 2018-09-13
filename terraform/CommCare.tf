507be021449c9865450a99a6f26dd2ade7bf7883
Michael Marucheck <mmarucheck@gmail.com>
2018-09-13 11:08:34 -0700

    more terraform

--- terraform/CommCare.tf
+++ terraform/CommCare.tf
@@ -1,17 +1,26 @@
 provider "aws" {
-    region = "us-west-2"
+  region = "us-west-2"
 }
 
 resource "aws_instance" "commcare" {
-     ami = "ami-51537029"
-     instance_type = "t2.medium"
-     root_block_device {
-         volume_size = 16
-     }
-     volume_tags {
-         Name = "commcare"
-     }
-     tags {
-         Name = "commcare"
-     }
+  ami           = "ami-51537029"
+  instance_type = "t2.medium"
+
+  security_groups = [
+    "default",
+    "ssh",
+    "http",
+  ]
+
+  root_block_device {
+    volume_size = 16
+  }
+
+  volume_tags {
+    Name = "commcare"
+  }
+
+  tags {
+    Name = "commcare"
+  }
 }
