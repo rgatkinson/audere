98add409ba60d7ad32627b522c374bbce42c33ab
Alex Kotliarskyi <alex.frantic@gmail.com>
2018-10-01 11:08:11 -0700

    Instructions about .env

--- learn/NodeJSWebService/src/util/config.ts
+++ learn/NodeJSWebService/src/util/config.ts
@@ -1,3 +1,9 @@
 import dotenv from "dotenv";
+import fs from "fs";
+import path from "path";
 
 dotenv.config();
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("Copy .env.example to .env and customize stuff :)");
+}
