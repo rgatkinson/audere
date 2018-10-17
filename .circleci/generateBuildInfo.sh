057199a4a398cfb07f0fa94986b5b638dd1d695d
Terri Paik <terri@auderenow.org>
2018-10-17 22:32:29 +0900

    Add buildInfo.json to top-level gitignore, add server side to build config

--- .circleci/generateBuildInfo.sh
+++ .circleci/generateBuildInfo.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env bash
 REPO_ROOT_DIR=$(git rev-parse --show-toplevel)
-PROJECT_DIR=$REPO_ROOT_DIR/learn/ReactNativeTS/FluTrack
+APP_DIR=$REPO_ROOT_DIR/learn/ReactNativeTS/FluTrack
+SERVER_DIR=$REPO_ROOT_DIR/learn/NodeJSWebService
 FILENAME=buildInfo.json
 TODAY=$(date +%Y%m%d)
 GITHASH=$(git log --pretty=format:'%h' -n 1)
 
-cat > $PROJECT_DIR/$FILENAME <<EOF
+cat > $APP_DIR/$FILENAME <<EOT
   {
     "buildInfo": {
       "date": "$TODAY",
       "hash": "$GITHASH"
     }
   }
-EOF
+EOT
 			  
+cat > $SERVER_DIR/$FILENAME <<EOT
+  {
+    "buildInfo": {
+      "date": "$TODAY",
+      "hash": "$GITHASH"
+    }
+  }
+EOT
