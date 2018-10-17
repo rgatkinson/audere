d5b4409be3d7ab7882a164906f09b9b7bacc119f
Alex Kotliarskyi <alex.frantic@gmail.com>
2018-10-16 21:13:57 -0700

    Use object spread in reducer

--- learn/ReactNativeTS/FluTrack/src/store/index.ts
+++ learn/ReactNativeTS/FluTrack/src/store/index.ts
@@ -4,18 +4,17 @@ import { SET_ID, SET_PASSWORD, SET_AGE } from "./Constants";
 const initialState = {
   id: "",
   password: "",
-  age: 0
+  age: 0,
 };
 
 const reducer = (state = initialState, action: any) => {
-  //console.log("reducer running", action);
   switch (action.type) {
     case SET_ID:
-      return Object.assign({}, state, { id: action.payload });
+      return { ...state, id: action.payload };
     case SET_PASSWORD:
-      return Object.assign({}, state, { password: action.payload });
+      return { ...state, password: action.payload };
     case SET_AGE:
-      return Object.assign({}, state, { age: action.payload });
+      return { ...state, age: action.payload };
     default:
       return state;
   }
