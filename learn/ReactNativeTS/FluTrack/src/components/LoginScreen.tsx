ac3417ee7bc557d11b818e34c85366c8e0a3c17d
Alex Kotliarskyi <alex.frantic@gmail.com>
2018-10-16 22:45:29 -0700

    Don't store login ID on login form
    Let's use redux-persist for more systematic approach

--- learn/ReactNativeTS/FluTrack/src/components/LoginScreen.tsx
+++ learn/ReactNativeTS/FluTrack/src/components/LoginScreen.tsx
@@ -30,64 +30,44 @@ class LoginScreen extends React.Component<Props> {
     screenHeight: height,
     screenWidth: width,
     appVersion: pjson.version,
-    isLoading: true,
-    idLoaded: false,
   };
 
-  componentWillMount() {
-    AsyncStorage.getItem("id").then(value => {
-      if (value !== null) {
-        this.setState({
-          id: JSON.parse(value),
-          idLoaded: JSON.parse(value).length != 0,
-        });
-      }
-      this.setState({ isLoading: false });
-    });
-  }
   render() {
-    if (this.state.isLoading) {
-      // Need this block so that the real render will not run until AsyncStorage fetch is complete
-      return <ScreenView />;
-    } else {
-      return (
-        <ScreenView>
-          <MyText size="title">
-            {pjson.name}
-            &trade;
-          </MyText>
-          <FieldLabel label="Login ID:">
-            <ValidatedInput
-              inputType="id"
-              defaultValue={this.state.id}
-              autoFocus={!this.state.idLoaded}
-              onChangeText={id => this.setState({ id })}
-              onSubmitEditing={() => {
-                this.passwordInput.focus();
-              }}
-            />
-          </FieldLabel>
-          <FieldLabel label="Password:">
-            <ValidatedInput
-              inputType="password"
-              autoFocus={this.state.idLoaded}
-              myRef={i => {
-                this.passwordInput = i;
-              }}
-              onChangeText={password => this.setState({ password })}
-            />
-          </FieldLabel>
-          <Button
-            title="LOGIN"
-            onPress={() => {
-              this.props.dispatch(logIn(this.state.id, this.state.password));
-              AsyncStorage.setItem("id", JSON.stringify(this.state.id));
-              interact(JSON.stringify(this.state));
+    return (
+      <ScreenView>
+        <MyText size="title">
+          {pjson.name}
+          &trade;
+        </MyText>
+        <FieldLabel label="Login ID:">
+          <ValidatedInput
+            inputType="id"
+            defaultValue={this.state.id}
+            autoFocus={true}
+            onChangeText={id => this.setState({ id })}
+            onSubmitEditing={() => {
+              this.passwordInput.focus();
             }}
           />
-        </ScreenView>
-      );
-    }
+        </FieldLabel>
+        <FieldLabel label="Password:">
+          <ValidatedInput
+            inputType="password"
+            myRef={i => {
+              this.passwordInput = i;
+            }}
+            onChangeText={password => this.setState({ password })}
+          />
+        </FieldLabel>
+        <Button
+          title="LOGIN"
+          onPress={() => {
+            this.props.dispatch(logIn(this.state.id, this.state.password));
+            interact(JSON.stringify(this.state));
+          }}
+        />
+      </ScreenView>
+    );
   }
 }
 
