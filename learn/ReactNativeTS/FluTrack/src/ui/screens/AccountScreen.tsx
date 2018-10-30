5e02404f9b5cf064e3ac8acf24cc10f06fd1017f
Terri Paik <terri@auderenow.org>
2018-10-30 09:14:46 +0900

    Revert "Add a stub About box to test OTA update via Expo publish"
    This reverts commit 9c28ccf82ccfcc3dce92b019eb15d0553deae552.

--- learn/ReactNativeTS/FluTrack/src/ui/screens/AccountScreen.tsx
+++ learn/ReactNativeTS/FluTrack/src/ui/screens/AccountScreen.tsx
@@ -6,7 +6,6 @@ import { logOut, StoreState } from "../../store";
 import { connect } from "react-redux";
 import { NavigationScreenProp } from "react-navigation";
 import Text from "../components/Text";
-import { Alert } from "react-native";
 
 interface Props {
   id: string;
@@ -26,12 +25,6 @@ export default class AccountScreen extends React.Component<Props> {
         <Text>Hello, {this.props.id}</Text>
         <Button title="START FORM" onPress={this.startForm} />
         <Button title="LOGOUT" onPress={this.logOut} />
-        <Button
-          title="ABOUT"
-          onPress={() => {
-            Alert.alert("About me!");
-          }}
-        />
       </ScreenView>
     );
   }
