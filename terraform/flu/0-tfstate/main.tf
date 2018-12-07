22480b33ca5290ab5f93900902531a1351ac8a66
Michael Marucheck <mmarucheck@gmail.com>
2018-12-07 09:19:06 -0800

    terraform: S3 logging, explicit acl, encrypt at rest
    This adds explicit acl="private", encrypts all s3 contents at rest with default kms key.  It creates a new bucket for each of staging and prod for logs and makes the terraform state buckets log there.

--- terraform/flu/0-tfstate/main.tf
+++ terraform/flu/0-tfstate/main.tf
@@ -9,24 +9,94 @@ provider "aws" {
 
 resource "aws_s3_bucket" "staging_terraform" {
   bucket = "flu-staging-terraform.auderenow.io"
+  acl = "private"
 
   versioning {
     enabled = true
   }
 
+  logging {
+    target_bucket = "${aws_s3_bucket.staging_log.bucket}"
+    target_prefix = "flu-staging-terraform-s3-bucket"
+  }
+
   lifecycle {
     prevent_destroy = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "${data.aws_kms_key.default.arn}"
+        sse_algorithm = "aws:kms"
+      }
+    }
   }
 }
 
 resource "aws_s3_bucket" "prod_terraform" {
   bucket = "flu-prod-terraform.auderenow.io"
+  acl = "private"
 
   versioning {
     enabled = true
   }
 
+  logging {
+    target_bucket = "${aws_s3_bucket.prod_log.bucket}"
+    target_prefix = "flu-prod-terraform-s3-bucket"
+  }
+
   lifecycle {
     prevent_destroy = true
   }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "${data.aws_kms_key.default.arn}"
+        sse_algorithm = "aws:kms"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket" "staging_log" {
+  bucket = "flu-staging-log.auderenow.io"
+  acl = "log-delivery-write"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "${data.aws_kms_key.default.arn}"
+        sse_algorithm = "aws:kms"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket" "prod_log" {
+  bucket = "flu-prod-log.auderenow.io"
+  acl = "log-delivery-write"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "${data.aws_kms_key.default.arn}"
+        sse_algorithm = "aws:kms"
+      }
+    }
+  }
+}
+
+data "aws_kms_key" "default" {
+  key_id = "arn:aws:kms:us-west-2:475613123583:key/dd2f00bc-0b07-4264-9f47-727510b3084f"
 }
