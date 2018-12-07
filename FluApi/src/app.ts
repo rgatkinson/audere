b7d4e589bc360839e12073bf6882ba9336487241
Michael Marucheck <mmarucheck@gmail.com>
2018-12-07 09:44:36 -0800

    Clean up ButtonPush demo leftover code

--- FluApi/src/app.ts
+++ FluApi/src/app.ts
@@ -7,10 +7,7 @@ import express from "express";
 import Ouch from "ouch";
 import bodyParser from "body-parser";
 import helmet from "helmet";
-import { promises as fs } from "fs";
 import * as DocumentsController from "./controllers/documentsController";
-import { ButtonPush } from "./models/buttonPush";
-import { ValidationError } from "sequelize";
 import { sequelize } from "./models";
 import { generateRandomKey } from "./util/crypto";
 import logger from "./util/logger";
@@ -23,51 +20,7 @@ app.set("port", process.env.PORT || 3000);
 app.use(helmet.noCache());
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("OK");
-});
-
-app.get("/boom", (req, res) => {
-  throw new Error("Boom!");
-});
-
-app.get(
-  "/async-boom",
-  wrap(async (req, res) => {
-    throw new Error("Async Boom!");
-  })
-);
-
-app.get("/api", (req, res) => {
-  res.json({
-    // TODO: Generate the token using `csrf` npm module
-    // Do we need CSRF for JSON API?
-    CsrfToken: null,
-    Status: "SUCCESS"
-  });
-});
-
-app.post(
-  "/api/button",
-  wrap(async (req, res) => {
-    try {
-      await ButtonPush.create({
-        deviceId: req.body.DeviceId,
-        timestamp: req.body.Timestamp,
-        count: req.body.Count,
-        extra: req.body.Extra
-      });
-      res.json({ Status: "SUCCESS" });
-    } catch (error) {
-      if (error instanceof ValidationError) {
-        res.status(400).json({ Status: error.message });
-      } else {
-        console.error(error);
-        throw error;
-      }
-    }
-  })
-);
+app.get("/api", (req, res) => res.json({ Status: "OK" }));
 
 app.put(
   "/api/documents/:key([A-Za-z0-9-_]{0,})/:documentId([A-Za-z0-9-_]{0,})",
--- FluApi/test/app.test.ts
+++ FluApi/test/app.test.ts
@@ -1,7 +1,6 @@
 import request from "supertest";
 import { promises as fs } from "fs";
 import app from "../src/app";
-import { ButtonPush } from "../src/models/buttonPush";
 import { sequelize } from "../src/models";
 
 afterAll(() => {
@@ -24,37 +23,6 @@ describe("GET /api", () => {
       .expect(200)
       .expect("content-type", /json/);
     expect(response.body.Status).toEqual("SUCCESS");
-  });
-});
-
-describe("POST /api/button", () => {
-  it("creates new record", async () => {
-    const response = await request(app)
-      .post("/api/button")
-      .send({
-        DeviceId: "53c86569-2f33-4829-945c-18b4718e2388",
-        Timestamp: "2018-09-26T03:40:25.693Z",
-        Count: 42
-      })
-      .expect(200)
-      .expect("content-type", /json/);
-    expect(response.body.Status).toEqual("SUCCESS");
-    const buttonPush: any = await ButtonPush.findOne({
-      where: {
-        deviceId: "53c86569-2f33-4829-945c-18b4718e2388",
-        timestamp: "2018-09-26T03:40:25.693Z"
-      }
-    });
-    expect(buttonPush.count).toEqual(42);
-  });
-
-  it("validates arguments", async () => {
-    const response = await request(app)
-      .post("/api/button")
-      .send({})
-      .expect(400)
-      .expect("content-type", /json/);
-    expect(response.body.Status).toMatch(/deviceId/);
   });
 });
 
