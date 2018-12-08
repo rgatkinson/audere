951c0814bb661e538752155232ee854901e717f7
Michael Marucheck <mmarucheck@gmail.com>
2018-12-07 16:08:00 -0800

    add ts type signatures for uploaded VisitDocument

--- lib/dist/types.d.ts
+++ lib/dist/types.d.ts
@@ -3,3 +3,104 @@ export declare enum DocumentType {
     Feedback = "FEEDBACK",
     Log = "LOG"
 }
+export declare type VisitDocument = {
+    csruid: string;
+    documentType: DocumentType.Visit;
+    device: DeviceInfo;
+    visit: VisitInfo;
+};
+export declare type DeviceInfo = {
+    installation: string;
+    clientVersion: string;
+    deviceName: string;
+    yearClass: string;
+    idiomText: string;
+    platform: string;
+};
+export declare type VisitInfo = {
+    complete: boolean;
+    gps_location?: GpsLocationInfo;
+    location?: string;
+    samples: SampleInfo[];
+    patient: PatientInfo;
+    consent: ConsentInfo;
+    responses: ResponseInfo[];
+    events: EventInfo[];
+};
+export declare type GpsLocationInfo = {
+    latitude: string;
+    longitude: string;
+};
+export declare type SampleInfo = {
+    sample_type: string;
+    code: string;
+};
+export declare type PatientInfo = {
+    name: string;
+    birthDate: string;
+    gender: "male" | "female" | "other" | "unknown";
+    telecom: TelecomInfo[];
+    address: AddressInfo[];
+};
+export declare type TelecomInfo = {
+    system: "phone" | "sms" | "email";
+    value: string;
+};
+export declare type AddressInfo = {
+    use: "home" | "work";
+    line: string[];
+    city: string;
+    state: string;
+    postalCode: string;
+    country: string;
+};
+export declare type ConsentInfo = {
+    terms: string;
+    name: string;
+    signerType: "Subject" | "Parent" | "Representative";
+    date: string;
+    signature: string;
+};
+export declare type ResponseInfo = {
+    id: string;
+    item: ResponseItemInfo[];
+};
+export declare type ResponseItemInfo = QuestionInfo & {
+    answer: AnswerInfo[];
+};
+export declare type QuestionInfo = {
+    id: string;
+    text: string;
+    answerOptions?: QuestionAnswerOption[];
+};
+export declare type QuestionAnswerOption = {
+    id: string;
+    text: string;
+};
+export declare type AnswerInfo = {
+    valueBoolean?: boolean;
+    valueDateTime?: string;
+    valueDecimal?: number;
+    valueInteger?: number;
+    valueString?: string;
+    valueAddress?: AddressValueInfo;
+    valueOther?: OtherValueInfo;
+    valueDeclined?: boolean;
+};
+export declare type AddressValueInfo = {
+    line: [string];
+    city: string;
+    state: string;
+    postalCode: string;
+    country: string;
+};
+export declare type OtherValueInfo = {
+    selectedIndex: Number;
+    valueString: string;
+};
+export declare type EventInfo = {
+    kind: "visit" | "response" | "sample";
+    at: string;
+    until: string;
+    refId?: string;
+};
--- lib/types.ts
+++ lib/types.ts
@@ -8,3 +8,148 @@ export enum DocumentType {
   Feedback = 'FEEDBACK',
   Log = 'LOG',
 }
+
+export type VisitDocument = {
+  // cryptographically secure unique id for this document.
+  csruid: string;
+
+  documentType: DocumentType.Visit;
+
+  // information about client device
+  device: DeviceInfo;
+
+  // clinical data related to the visit
+  visit: VisitInfo;
+}
+
+export type DeviceInfo = {
+  installation: string; // uuid
+  clientVersion: string;
+  deviceName: string;
+  yearClass: string;
+  idiomText: string;
+  platform: string;
+}
+
+export type VisitInfo = {
+  complete: boolean;
+  gps_location?: GpsLocationInfo;
+  location?: string;
+  samples: SampleInfo[];
+  patient: PatientInfo;
+  consent: ConsentInfo;
+  responses: ResponseInfo[];
+  events: EventInfo[];
+}
+
+export type GpsLocationInfo = {
+  latitude: string;
+  longitude: string;
+}
+
+// Information about swabs or other physical samples collected during visit
+export type SampleInfo = {
+  // Possible values TBD
+  sample_type: string;
+  // Value read from the test kit's QR code, or another unique identifier
+  code: string;
+}
+
+// This is a subset of the FHIR 'Patient' resource
+// https://www.hl7.org/fhir/patient.html
+export type PatientInfo = {
+  name: string;
+  birthDate: string; // FHIR:date
+
+  // The following options come from:
+  // https://www.hl7.org/fhir/valueset-administrative-gender.html
+  gender: "male" | "female" | "other" | "unknown";
+
+  telecom: TelecomInfo[];
+  address: AddressInfo[];
+}
+
+export type TelecomInfo = {
+  system: "phone" | "sms" | "email";
+  value: string;
+}
+
+export type AddressInfo = {
+  use: "home" | "work";
+  line: string[];
+  city: string;
+  state: string;
+  postalCode: string;
+  country: string;
+}
+
+export type ConsentInfo = {
+  terms: string;
+  name: string;
+  signerType: "Subject" | "Parent" | "Representative";
+  date: string; // date only
+  signature: string; // Base64-encoded PNG of the signature
+}
+
+// This is loosely based on the FHIR 'QuestionnaireResponse' resource
+// https://www.hl7.org/fhir/questionnaireresponse.html
+export type ResponseInfo = {
+  id: string;
+  item: ResponseItemInfo[];
+}
+
+export type ResponseItemInfo = QuestionInfo & { answer: AnswerInfo[]; }
+
+export type QuestionInfo = {
+  // human-readable, locale-independent id of the question
+  id: string;
+  // localized text of question
+  text: string;
+  // For multiple-choice questions, the exact text of each option, in order
+  answerOptions?: QuestionAnswerOption[];
+}
+
+export type QuestionAnswerOption = {
+  id: string;
+  text: string;
+}
+
+export type AnswerInfo = {
+  valueBoolean?: boolean;
+  valueDateTime?: string; // FHIR:dateTime
+  valueDecimal?: number;
+  valueInteger?: number;
+  valueString?: string;
+  valueAddress?: AddressValueInfo;
+
+  // If the selected option also has a freeform text box, e.g
+  // 'Other, please specify: _________'
+  valueOther?: OtherValueInfo;
+
+  // True if the patiented declined to respond to the question
+  valueDeclined?: boolean;
+}
+
+export type AddressValueInfo = {
+  line: [string];
+  city: string;
+  state: string;
+  postalCode: string;
+  country: string;
+}
+
+export type OtherValueInfo = {
+  // Index in answerOptions of the selected choice
+  selectedIndex: Number;
+  valueString: string;
+}
+
+export type EventInfo = {
+  kind: "visit" | "response" | "sample";
+
+  at: string; // FHIR:instant
+  until: string; // FHIR:instant
+
+  // id of the item this event describes (e.g. question id), if applicable
+  refId?: string;
+}
