e02d71d4ab9d18334afbe13baf85480f9c59190a
Jenny Abrahamson <jenny.abrahamson@gmail.com>
2018-12-11 11:15:18 -0800

    Fix redux persistence for maps

--- learn/ReactNativeTS/FluTrack/src/store/index.ts
+++ learn/ReactNativeTS/FluTrack/src/store/index.ts
@@ -1,6 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import immutableTransform from 'redux-persist-transform-immutable'
 
 import { uploaderMiddleware } from "./uploader";
 
@@ -17,10 +18,12 @@ export type Action = UserAction | FormAction | AdminAction;
 
 export { StoreState } from "./StoreState";
 
-const config = {
+const persistConfig = {
+  transforms: [immutableTransform()],
   key: "store",
   storage,
 };
+
 
 const reducer = combineReducers({
   user,
@@ -29,7 +32,7 @@ const reducer = combineReducers({
 });
 
 export const store = createStore(
-  persistReducer(config, reducer),
+  persistReducer(persistConfig, reducer),
   applyMiddleware(uploaderMiddleware)
 );
 export const persistor = persistStore(store);
