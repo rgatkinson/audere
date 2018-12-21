91ab62852c5d02a246f85a69f37bdc9e32491077
Terri Paik <terri@auderenow.org>
2018-12-21 13:19:34 +0900

    #258 Year selector for vaccination question shouldn't include 2 years ago

--- learn/ReactNativeTS/FluTrack/src/ui/components/DateModal.tsx
+++ learn/ReactNativeTS/FluTrack/src/ui/components/DateModal.tsx
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  DatePickerIOS,
-  Picker,
-  StyleSheet,
-  View,
-} from "react-native";
+import { DatePickerIOS, Picker, StyleSheet, View } from "react-native";
 import { WithNamespaces, withNamespaces } from "react-i18next";
 import Modal from "./Modal";
 
@@ -34,11 +29,11 @@ class DateModal extends React.Component<Props & WithNamespaces> {
   state = {
     selectedDate: false,
     date: new Date(),
-  }
+  };
 
   _getDate = (): Date => {
-    return this.state.selectedDate ? this.state.date :  this.props.date;
-  }
+    return this.state.selectedDate ? this.state.date : this.props.date;
+  };
 
   render() {
     const { t } = this.props;
@@ -46,51 +41,54 @@ class DateModal extends React.Component<Props & WithNamespaces> {
       <Modal
         height={280}
         width={350}
-        submitText={t('common:button:done')}
+        submitText={t("common:button:done")}
         visible={this.props.visible}
         onDismiss={() => this.props.onDismiss(this.props.date)}
         onSubmit={() => this.props.onDismiss(this._getDate())}
       >
-        {this.props.mode === "day"
-          ? <DatePickerIOS
-              date={this._getDate()}
-              maximumDate={new Date()}
-              mode="date"
-              onDateChange={(date) => {
-                this.setState({ selectedDate: true, date})
+        {this.props.mode === "day" ? (
+          <DatePickerIOS
+            date={this._getDate()}
+            maximumDate={new Date()}
+            mode="date"
+            onDateChange={date => {
+              this.setState({ selectedDate: true, date });
+            }}
+            timeZoneOffsetInMinutes={0} // force to UTC
+          />
+        ) : (
+          <View style={styles.pickerContainer}>
+            <Picker
+              selectedValue={months[this._getDate().getMonth()]}
+              style={{ height: 50, width: 250 }}
+              onValueChange={month => {
+                const newDate = new Date(this._getDate());
+                newDate.setMonth(months.indexOf(month));
+                this.setState({ selectedDate: true, date: newDate });
               }}
-              timeZoneOffsetInMinutes={0} // force to UTC
-            />
-          : <View style={styles.pickerContainer}>
-              <Picker
-                selectedValue={months[this._getDate().getMonth()]}
-                style={{ height: 50, width: 250 }}
-                onValueChange={(month) => {
-                  const newDate = new Date(this._getDate());
-                  newDate.setMonth(months.indexOf(month));
-                  this.setState({ selectedDate: true, date: newDate });
-                }}>
-                {months.map(month => (
-                  <Picker.Item label={t(month)} value={month} key={month} />
-                ))}
-              </Picker>
-              <Picker
-                selectedValue={this._getDate().getFullYear()}
-                style={{ height: 50, width: 100 }}
-                onValueChange={year => {
-                  const newDate = new Date(this._getDate());
-                  newDate.setFullYear(year);
-                  this.setState({ selectedDate: true, date: newDate });
-                }}>
-                {[...Array(3).keys()].reverse().map(index => {
-                  const year = (new Date()).getFullYear() - index;
-                  return (
-                    <Picker.Item label={'' + year} value={year} key={year} />
-                  );
-                })}
-              </Picker>
-            </View>
-        }
+            >
+              {months.map(month => (
+                <Picker.Item label={t(month)} value={month} key={month} />
+              ))}
+            </Picker>
+            <Picker
+              selectedValue={this._getDate().getFullYear()}
+              style={{ height: 50, width: 100 }}
+              onValueChange={year => {
+                const newDate = new Date(this._getDate());
+                newDate.setFullYear(year);
+                this.setState({ selectedDate: true, date: newDate });
+              }}
+            >
+              {[...Array(2).keys()].reverse().map(index => {
+                const year = new Date().getFullYear() - index;
+                return (
+                  <Picker.Item label={"" + year} value={year} key={year} />
+                );
+              })}
+            </Picker>
+          </View>
+        )}
       </Modal>
     );
   }
@@ -98,9 +96,9 @@ class DateModal extends React.Component<Props & WithNamespaces> {
 
 const styles = StyleSheet.create({
   pickerContainer: {
-    alignItems: 'center',
-    flexDirection: 'row',
-    justifyContent: 'center'
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "center",
   },
 });
 
