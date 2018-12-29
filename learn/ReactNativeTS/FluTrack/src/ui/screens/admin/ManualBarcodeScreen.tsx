3cc74a4873b7be9bdd048de271fb1d843557cd5f
Terri Paik <terri@auderenow.org>
2018-12-29 12:43:26 +0900

    Per Izzy 12.28 change barcode keyboard to numeric

--- learn/ReactNativeTS/FluTrack/src/ui/screens/admin/ManualBarcodeScreen.tsx
+++ learn/ReactNativeTS/FluTrack/src/ui/screens/admin/ManualBarcodeScreen.tsx
@@ -104,6 +104,7 @@ export default class ManualBarcodeScreen extends React.Component<Props> {
         <View style={styles.inputContainer}>
           <TextInput
             autoFocus={true}
+            keyboardType="numbers-and-punctuation"
             placeholder="Enter barcode data"
             returnKeyType="next"
             value={this.state.barcode1}
@@ -113,6 +114,7 @@ export default class ManualBarcodeScreen extends React.Component<Props> {
             onSubmitEditing={() => this.confirmInput.current!.focus()}
           />
           <TextInput
+            keyboardType="numbers-and-punctuation"
             placeholder="Confirm barcode data"
             ref={this.confirmInput}
             returnKeyType="done"
