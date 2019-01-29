f73b165837bbd4441835e7e32794ced2e2152753
Michael Marucheck <mmarucheck@gmail.com>
2019-01-28 16:34:21 -0800

    FluApi: disable global SQL logging

--- FluApi/src/models/index.ts
+++ FluApi/src/models/index.ts
@@ -3,16 +3,16 @@
 // Use of this source code is governed by an MIT-style license that
 // can be found in the LICENSE file distributed with this file.
 
-import fs from "fs";
-import path from "path";
 import Sequelize from "sequelize";
 import "../util/config";
-import sequelizeLogger from "sequelize-log-syntax-colors";
+// import sequelizeLogger from "sequelize-log-syntax-colors";
 
 export const sequelizeNonPII = new Sequelize(process.env.NONPII_DATABASE_URL, {
-  logging: (str: string) => console.log(sequelizeLogger(str))
+  //logging: (str: string) => console.log(sequelizeLogger(str))
+  logging: false,
 });
 
 export const sequelizePII = new Sequelize(process.env.PII_DATABASE_URL, {
-  logging: (str: string) => console.log(sequelizeLogger(str))
+  //logging: (str: string) => console.log(sequelizeLogger(str))
+  logging: false,
 });
