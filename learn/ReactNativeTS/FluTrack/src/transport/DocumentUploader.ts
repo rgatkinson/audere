57ffb21c2f61156fb26dde5896fec93d9de7861e
Michael Marucheck <mmarucheck@gmail.com>
2019-02-05 17:10:31 -0800

    ClientLogging: env var for record limit, log Pouch contents

--- learn/ReactNativeTS/FluTrack/src/transport/DocumentUploader.ts
+++ learn/ReactNativeTS/FluTrack/src/transport/DocumentUploader.ts
@@ -300,7 +300,9 @@ export class DocumentUploader {
         row.doc._id.startsWith("csruid/documents/")
       ).length;
       this.logger.debug(
-        `Pouch contents: ${total} entries, of which ${docs} docs and ${csruids} csruids`
+        `Pouch contents: ` +
+          `${total} entries, of which ${docs} docs and ${csruids} csruids` +
+          items.rows.map((row: any) => `\n  ${row.doc._id}`)
       );
     } else {
       this.logger.debug("Pouch contents: no items found");
