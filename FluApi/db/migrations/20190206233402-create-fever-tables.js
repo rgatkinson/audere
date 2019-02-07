aa84dad6bc49dc2a7bf984642145779b0657b9f8
Michael Marucheck <mmarucheck@gmail.com>
2019-02-06 21:11:57 -0800

    FeverEndpoint checkpoint

--- FluApi/db/migrations/20190206233402-create-fever-tables.js
+++ FluApi/db/migrations/20190206233402-create-fever-tables.js
@@ -1,77 +1,112 @@
-'use strict';
+// Copyright (c) 2019 by Audere
+//
+// Use of this source code is governed by an MIT-style license that
+// can be found in the LICENSE file distributed with this file.
+
+"use strict";
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return Promise.all(
       queryInterface.createTable("fever_screenings", {
-        ...basicTable(),
+        id: idColumn(),
+        createdAt: dateColumn(),
+        updatedAt: dateColumn(),
         csruid: unique(stringColumn()),
         device: jsonColumn(),
-        screening: jsonColumn(),
+        screenings: jsonColumn()
+      }),
+      queryInterface.createTable("fever_screenings_backups", {
+        id: idColumn(),
+        createdAt: dateColumn(),
+        updatedAt: dateColumn(),
+        csruid: unique(stringColumn()),
+        device: jsonColumn(),
+        screenings: jsonColumn()
+      }),
+      queryInterface.createTable("fever_surveys", {
+        id: idColumn(),
+        createdAt: dateColumn(),
+        updatedAt: dateColumn(),
+        csruid: unique(stringColumn()),
+        device: jsonColumn(),
+        surveys: jsonColumn()
+      }),
+      queryInterface.createTable("fever_surveys_backups", {
+        id: idColumn(),
+        createdAt: dateColumn(),
+        updatedAt: dateColumn(),
+        csruid: unique(stringColumn()),
+        device: jsonColumn(),
+        surveys: jsonColumn()
       }),
       queryInterface.createTable("fever_access_keys", {
-        ...basicTable(),
+        id: idColumn(),
+        createdAt: dateColumn(),
+        updatedAt: dateColumn(),
         key: stringColumn(),
-        valid: booleanColumn(),
+        valid: booleanColumn()
+      }),
+      queryInterface.createTable("fever_client_logs", {
+        id: idColumn(),
+        createdAt: dateColumn(),
+        updatedAt: dateColumn(),
+        device: jsonColumn(),
+        level: integerColumn(),
+        log: stringColumn()
+      }),
+      queryInterface.createTable("fever_client_log_batches", {
+        id: idColumn(),
+        createdAt: dateColumn(),
+        updatedAt: dateColumn(),
+        csruid: unique(stringColumn()),
+        device: jsonColumn(),
+        surveys: jsonColumn()
       })
     );
   },
 
   down: (queryInterface, Sequelize) => {
     return Promise.all(
-      queryInterface.dropTable("fever_screenings"),
+      [
+        "fever_screenings",
+        "fever_screenings_backups",
+        "fever_surveys",
+        "fever_surveys_backups",
+        "fever_access_keys",
+        "fever_client_logs",
+        "fever_client_log_batches"
+      ].map(queryInterface.dropTable)
     );
   }
 };
 
-function basicTable() {
-  return {
-    id: idColumn(),
-    createdAt: dateColumn(),
-    updatedAt: dateColumn(),
-  };
+function unique(column) {
+  return { ...column, unique: true };
 }
-
+function stringColumn() {
+  return column(Sequelize.STRING);
+}
+function booleanColumn() {
+  return column(Sequelize.BOOLEAN);
+}
+function jsonColumn() {
+  return column(Sequelize.JSON);
+}
+function integerColumn() {
+  return column(Sequelize.INTEGER);
+}
+function dateColumn() {
+  return column(Sequelize.DATE);
+}
+function column(type) {
+  return { allowNull: false, type };
+}
 function idColumn() {
   return {
     allowNull: false,
     autoIncrement: true,
     primaryKey: true,
     type: Sequelize.INTEGER
-  };
-}
-
-function unique(column) {
-  return {
-    ...column,
-    unique: true
-  }
-}
-
-function stringColumn() {
-  return {
-    allowNull; false,
-    type: Sequelize.STRING
-  };
-}
-
-function booleanColumn() {
-  return {
-    allowNull: false,
-    type: Sequelize.BOOLEAN
-  };
-}
-
-function jsonColumn() {
-  return {
-    allowNull: false,
-    type: Sequelize.JSON
-  };
-}
-
-function dateColumn() {
-  return {
-    allowNull: false,
-    type: Sequelize.DATE
   };
 }
