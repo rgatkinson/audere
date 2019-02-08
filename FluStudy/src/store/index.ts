d8f6353d1c932e286dcdd9e07db09acef4ef7c85
Ram Kandasamy <ram.kandasamy@gmail.com>
2019-02-07 16:53:15 -0800

    Encrypt persisted redux contents (#434)
    * Encrypt persisted redux contents for sniffles
    
    * Encrypt persisted redux contents for fever

--- FluStudy/src/store/index.ts
+++ FluStudy/src/store/index.ts
@@ -1,4 +1,4 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createStore, combineReducers, applyMiddleware, Store } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import {
   createReactNavigationReduxMiddleware,
@@ -6,6 +6,7 @@ import {
 } from "react-navigation-redux-helpers";
 import { NavigationAction } from "react-navigation";
 import storage from "redux-persist/lib/storage";
+import createEncryptor from "redux-persist-transform-encrypt";
 import immutableTransform from "redux-persist-transform-immutable";
 import { uploaderMiddleware } from "./uploader";
 
@@ -23,6 +24,8 @@ export * from "./navigation";
 import { default as survey, SurveyState, SurveyAction } from "./survey";
 export * from "./survey";
 
+import { uploader } from "./uploader";
+
 export type Action = MetaAction | ScreeningAction | SurveyAction;
 
 export type Action =
@@ -34,20 +37,41 @@ export type Action =
 import { StoreState } from "./StoreState";
 export { StoreState } from "./StoreState";
 
-const persistConfig = {
-  transforms: [immutableTransform()],
-  key: "store",
-  storage,
-};
-
 const reducer = combineReducers({
   meta,
   navigation,
   survey,
 });
 
-export const store = createStore(
-  persistReducer(persistConfig, reducer),
-  applyMiddleware(uploaderMiddleware)
-);
-export const persistor = persistStore(store);
+let storePromise: Promise<Store>;
+export function getStore(): Promise<Store> {
+  if (storePromise) {
+    return storePromise;
+  }
+  return (storePromise = getStoreImpl());
+}
+
+async function getStoreImpl() {
+  const password = await uploader.getEncryptionPassword();
+  const persistConfig = {
+    transforms: [
+      createEncryptor({
+        secretKey: password,
+        onError(e: Error) {
+          console.error(e);
+        },
+      }),
+      immutableTransform(),
+    ],
+    key: "store",
+    storage,
+  };
+  return createStore(
+    persistReducer(persistConfig, reducer),
+    applyMiddleware(uploaderMiddleware)
+  );
+}
+
+export async function getPersistor() {
+  return persistStore(await getStore());
+}
--- learn/ReactNativeTS/FluTrack/src/util/formTimeout.ts
+++ learn/ReactNativeTS/FluTrack/src/util/formTimeout.ts
@@ -4,23 +4,28 @@
 // can be found in the LICENSE file distributed with this file.
 
 import { NavigationScreenProp } from "react-navigation";
-import { clearForm, completeSurvey, store } from "../store";
+import { clearForm, completeSurvey, StoreState } from "../store";
 
 const TWO_HOURS_MS = 2 * 60 * 60 * 1000;
 
 export function completeFormIfExpired(
+  state: StoreState,
+  dispatch: Function,
   navigation: NavigationScreenProp<any, any>
 ) {
-  const completed = store.getState().form.completedSurvey;
-  const timestamp = store.getState().form.timestamp;
+  if (!state.form) {
+    return;
+  }
+  const completed = state.form.completedSurvey;
+  const timestamp = state.form.timestamp;
   if (!completed && !!timestamp) {
     // We have a form in-progress
     const currentIntervalMs = new Date().getTime() - timestamp;
 
     if (currentIntervalMs >= TWO_HOURS_MS) {
       // This form has already expired, we should clear it
-      store.dispatch(completeSurvey());
-      store.dispatch(clearForm());
+      dispatch(completeSurvey());
+      dispatch(clearForm());
       navigation.popToTop();
     }
   }
