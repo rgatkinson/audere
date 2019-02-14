7c6e1a3ac6cb3ee950243f5caef0b09a79eb7006
Jenny Abrahamson <jenny.abrahamson@gmail.com>
2019-02-14 09:13:13 -0800

    Update about screen with build info

--- FluStudy/src/ui/screens/AboutScreen.tsx
+++ FluStudy/src/ui/screens/AboutScreen.tsx
@@ -4,14 +4,28 @@
 // can be found in the LICENSE file distributed with this file.
 
 import React from "react";
-import { Dimensions, Image, View, StyleSheet, Switch } from "react-native";
+import {
+  Clipboard,
+  Dimensions,
+  Image,
+  Platform,
+  View,
+  StyleSheet,
+  Switch,
+} from "react-native";
 import { NavigationScreenProp } from "react-navigation";
 import { WithNamespaces, withNamespaces } from "react-i18next";
+import { Constants } from "expo";
 import { connect } from "react-redux";
+import { getApiBaseUrl } from "../../transport";
 import { Action, setDemo, StoreState } from "../../store";
+import Button from "../components/Button";
 import Screen from "../components/Screen";
+import Text from "../components/Text";
 import Title from "../components/Title";
 import { GUTTER, STATUS_BAR_HEIGHT } from "../styles";
+
+const buildInfo = require("../../../buildInfo.json");
 
 interface Props {
   demoMode: boolean;
@@ -23,7 +37,30 @@ interface Props {
   demoMode: state.meta.isDemo,
 }))
 class AboutScreen extends React.PureComponent<Props & WithNamespaces> {
+  copyToClipboard = async (text: string) => {
+    await Clipboard.setString(text);
+  };
+
   render() {
+    const aboutContent: string =
+      "**" +
+      buildInfo.name +
+      "**" +
+      "\n**Version:** " +
+      buildInfo.version +
+      "\n**Commit:** " +
+      buildInfo.hash +
+      "\n**Date:** " +
+      buildInfo.buildDate +
+      "\n**Device:** " +
+      Platform.OS +
+      " " +
+      Platform.Version +
+      "\n**Installation:** " +
+      Constants.installationId +
+      "\n**API Server:** " +
+      getApiBaseUrl();
+
     const { t } = this.props;
     return (
       <View style={{ marginTop: STATUS_BAR_HEIGHT }}>
@@ -39,6 +76,16 @@ class AboutScreen extends React.PureComponent<Props & WithNamespaces> {
               onValueChange={value => this.props.dispatch(setDemo(value))}
             />
           </View>
+          <Text content={aboutContent} />
+          <Button
+            label="Copy"
+            primary={true}
+            enabled={true}
+            style={{ marginTop: GUTTER }}
+            onPress={() => {
+              this.copyToClipboard(aboutContent);
+            }}
+          />
         </View>
       </View>
     );
