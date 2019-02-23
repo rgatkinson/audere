dda9d469f53f9255fd483f8b0f360a7cc67f2ace
Michael Marucheck <mmarucheck@gmail.com>
2019-02-23 13:36:20 -0800

    Fix red screen of doom - remove crypto

--- FluStudy/src/util/csruid.ts
+++ FluStudy/src/util/csruid.ts
@@ -3,12 +3,9 @@
 // Use of this source code is governed by an MIT-style license that
 // can be found in the LICENSE file distributed with this file.
 
-import crypto from "crypto";
-import util from "util";
-
-const randomBytes = util.promisify(crypto.randomBytes);
+// TODO - needs to be cryptographically secure
+import uuidv4 from "uuid/v4";
 
 export async function newCSRUID(): Promise<string> {
-  const bytes = await randomBytes(64);
-  return bytes.toString("hex");
+  return uuidv4();
 }
