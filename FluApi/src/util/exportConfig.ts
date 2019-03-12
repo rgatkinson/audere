f7301304cc253bb9a30701b49dc611e90dfc5b9b
billy-audere <45672302+billy-audere@users.noreply.github.com>
2019-03-12 11:36:47 -0700

    Decreasing batch size so geocoding quota isn't excessively consumed (#703)

--- FluApi/src/util/exportConfig.ts
+++ FluApi/src/util/exportConfig.ts
@@ -5,7 +5,7 @@
 
 import { SecretConfig } from "./secretsConfig";
 
-export const defaultNumEncounters: number = +process.env.NUM_ENCOUNTERS || 500;
+export const defaultNumEncounters: number = +process.env.NUM_ENCOUNTERS || 100;
 
 export const hutchConcurrentUploads: number =
   +process.env.HUTCH_CONCURRENT_UPLOADS || 50;
