cada79a15f8e90c4931085f02ae727957c762a18
Michael Marucheck <mmarucheck@gmail.com>
2019-03-19 22:12:23 -0700

    @balderdash feedback

--- FluApi/src/endpoints/webPortal/auth.ts
+++ FluApi/src/endpoints/webPortal/auth.ts
@@ -3,11 +3,8 @@
 // Use of this source code is governed by an MIT-style license that
 // can be found in the LICENSE file distributed with this file.
 
-import fs from "fs";
-import util from "util";
 import crypto from "crypto";
 import uuidv4 from "uuid/v4";
-import { Op } from "sequelize";
 import passport, { Passport } from "passport";
 import { Strategy as LocalStrategy } from "passport-local";
 import {Inst, SplitSql} from "../../util/sql";
@@ -64,23 +61,13 @@ export class AuthManager {
   async setPassword(userid: string, password: string): Promise<void> {
     const user = await this.findUser(userid);
     const salt = await makeSecret();
-    await this.models.user.update({
-      uuid: user.uuid,
-      userid,
-      salt,
-      token: makeToken({ salt, userid, password }),
-    });
+    await user.update({ salt, token: makeToken({ salt, userid, password })});
   }
 
   async disableUser(userid: string): Promise<void> {
     const user = await this.findUser(userid);
     const salt = await makeSecret();
-    await this.models.user.update({
-      uuid: user.uuid,
-      userid,
-      salt,
-      token: "",
-    });
+    await user.update({ salt, token: ""});
   }
 
   async deleteUser(userid: string): Promise<void> {
@@ -97,26 +84,8 @@ export class AuthManager {
   }
 }
 
-const fsOpen = util.promisify(fs.open);
-const fsRead = util.promisify(fs.read);
-const fsClose = util.promisify(fs.close);
-
 async function makeSecret(size: number = 64): Promise<string> {
-  const buffer = Buffer.alloc(size);
-  const fd = await fsOpen("/dev/urandom", "r");
-  try {
-    let count = 0;
-    while (count < size) {
-      const read = await fsRead(fd, buffer, count, size - count, null);
-      if (read.bytesRead < 1) {
-        throw new Error(`Got ${read.bytesRead} from read of /dev/urandom`);
-      }
-      count += read.bytesRead;
-    }
-    return hash(buffer);
-  } finally {
-    await fsClose(fd);
-  }
+  return crypto.randomBytes(size).toString("base64");
 }
 
 interface TokenParts {
@@ -124,8 +93,7 @@ interface TokenParts {
   userid: string;
   password: string;
 }
-function makeToken(parts: TokenParts): string {
-  const { salt, userid, password } = parts;
+function makeToken({ salt, userid, password }: TokenParts): string {
   return hash(salt, userid, password);
 }
 
