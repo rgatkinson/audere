e17bf904c7b5443331c9a7e5908d565eb420460b
billy-audere <45672302+billy-audere@users.noreply.github.com>
2019-03-21 18:13:04 -0700

    Applying list bucket permission to the bucket instead of its contents (#767)

--- terraform/modules/flu-api/policy.tf
+++ terraform/modules/flu-api/policy.tf
@@ -68,11 +68,18 @@ data "aws_iam_policy_document" "flu_api_s3_policy" {
     actions = [
       "s3:PutObject",
       "s3:DeleteObject",
-      "s3:ListBucket",
       "s3:GetObject"
     ]
 
     resources = ["${aws_s3_bucket.flu_api_reports_bucket.arn}/*"]
+  }
+
+  statement {
+    actions = [
+      "s3:ListBucket"
+    ]
+
+    resources = ["${aws_s3_bucket.flu_api_reports_bucket.arn}"]
   }
 }
 
