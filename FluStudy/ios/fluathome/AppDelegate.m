7abeacb413cf14742deba716089f3167da8e31d7
Jenny Abrahamson <jenny.abrahamson@gmail.com>
2019-03-26 15:39:13 -0700

    Update expo sdk to version 32 (#776)

--- FluStudy/ios/fluathome/AppDelegate.m
+++ FluStudy/ios/fluathome/AppDelegate.m
@@ -4,7 +4,6 @@
 #import "ExpoKit.h"
 #import "EXViewController.h"
 #import <Firebase.h>
-#import "RCTPushNotificationManager.h"
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
 #import <Crashlytics/Crashlytics.h>
 #import "RNFirebaseLinks.h"
@@ -23,21 +22,15 @@
     [FIRApp configure];
     [[FBSDKApplicationDelegate sharedInstance] application:application
                              didFinishLaunchingWithOptions:launchOptions];
-    
-    _window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-    _window.backgroundColor = [UIColor whiteColor];
     [[ExpoKit sharedInstance] application:application didFinishLaunchingWithOptions:launchOptions];
     _rootViewController = [ExpoKit sharedInstance].rootViewController;
-    _window.rootViewController = _rootViewController;
-
-    [_window makeKeyAndVisible];
-    
-    return YES;
+    return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
-- (void)applicationDidBecomeActive:(UIApplication *)application {
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
     [FBSDKAppEvents activateApp];
-
+    
     // Uncomment below if you'd like to force a crash to see if it'll show up in Crashlytics on Firebase.
     // Yes, the rand bit is lame -- you should feel free to use your own method.  But basically, after
     // a crash, you need a way to get the app to run again WITHOUT crashing, at least during launch,
@@ -48,38 +41,43 @@
     // if (r != 0) {
     //  [[Crashlytics sharedInstance] crash];
     // }
+    [super applicationWillEnterForeground:application];
+}
+
+#pragma mark - Background Fetch
+
+- (void)application:(UIApplication *)application performFetchWithCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
+{
+    [super application:application performFetchWithCompletionHandler:completionHandler];
 }
 
 #pragma mark - Handling URLs
-
-- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(nullable NSString *)sourceApplication annotation:(id)annotation
+- (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler
 {
-    return [[FBSDKApplicationDelegate sharedInstance] application:application
-                                                          openURL:url
-                                                sourceApplication:sourceApplication
-                                                       annotation:annotation];
-}
-
-- (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity restorationHandler:(nonnull void (^)(NSArray * _Nullable))restorationHandler
-{
-    BOOL handled = [[ExpoKit sharedInstance] application:application continueUserActivity:userActivity restorationHandler:restorationHandler];
+    BOOL handled = [[RNFirebaseLinks instance] application:application continueUserActivity:userActivity restorationHandler:restorationHandler];
 
     if (!handled) {
-        handled = [[RNFirebaseLinks instance] application:application continueUserActivity:userActivity restorationHandler:restorationHandler];
+        handled = [super application:application continueUserActivity:userActivity restorationHandler:restorationHandler];
     }
     return handled;
 }
 
-- (BOOL)application:(UIApplication *)application
+- (BOOL)application:(UIApplication *)app
             openURL:(NSURL *)url
-            options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
-    BOOL handled = [[FBSDKApplicationDelegate sharedInstance] application:application
+            options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+    BOOL handled = [[FBSDKApplicationDelegate sharedInstance] application:app
                                                                   openURL:url
                                                         sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]
                                                                annotation:options[UIApplicationOpenURLOptionsAnnotationKey]
                     ];
+    
     if (!handled) {
-        handled = [[RNFirebaseLinks instance] application:application openURL:url options:options];
+        handled = [[RNFirebaseLinks instance] application:app openURL:url options:options];
+    }
+
+    if (!handled) {
+        handled = [super application:app openURL:url options:options];
     }
 
     return handled;
@@ -87,32 +85,19 @@
 
 #pragma mark - Notifications
 
-// Required to register for notifications
-- (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)token
 {
-    [RCTPushNotificationManager didRegisterUserNotificationSettings:notificationSettings];
-}
-// Required for the register event.
-- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
-{
-    [RCTPushNotificationManager didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
-}
-// Required for the notification event. You must call the completion handler after handling the remote notification.
-- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
-fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
-{
-    [RCTPushNotificationManager didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
-}
-// Required for the registrationError event.
-- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
-{
-    [RCTPushNotificationManager didFailToRegisterForRemoteNotificationsWithError:error];
-}
-// Required for the localNotification event.
-- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification
-{
-    [RCTPushNotificationManager didReceiveLocalNotification:notification];
+    [super application:application didRegisterForRemoteNotificationsWithDeviceToken:token];
 }
 
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)err
+{
+    [super application:application didFailToRegisterForRemoteNotificationsWithError:err];
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult result))completionHandler
+{
+    [super application:application didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
+}
 
 @end
