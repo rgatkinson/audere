17e98003ec3719e56f52409e47e85583954d0d89
billy-audere <45672302+billy-audere@users.noreply.github.com>
2019-04-22 16:28:14 -0700

    Changing reports schedule for courier delivery of kits (#987)

--- terraform/modules/flu-lambda/main.tf
+++ terraform/modules/flu-lambda/main.tf
@@ -10,6 +10,7 @@ locals {
   // This is 8:30 AM and 1:30 PM local in PST
   // See: https://docs.aws.amazon.com/AmazonCloudWatch/latest/events/ScheduledEvents.html
   cron_weekdays_before_9AM_and_1PM_PST = "cron(30 15,19 ? * MON-FRI *)"
+  cron_weekdays_before_1PM_PST = "cron(30 19 ? * MON-FRI *)"
   cron_weekdays_at_4AM_PST = "cron(0 10 ? * MON-FRI *)"
   cron_monday_thursday_before_9AM_PST = "cron(30 15 ? * MON,THU *)"
 }
@@ -125,7 +126,7 @@ module "fever_kits_report_cron" {
   source = "../lambda-cron"
   name = "${local.base_name}-fever-kits-report"
   role_arn = "${aws_iam_role.flu_lambda.arn}"
-  frequency = "${local.cron_weekdays_before_9AM_and_1PM_PST}"
+  frequency = "${local.cron_weekdays_before_1PM_PST}"
   url = "http://${var.fluapi_fqdn}:444/api/export/sendKitOrders"
   subnet_id = "${var.lambda_subnet_id}"
   security_group_ids = ["${var.internal_elb_access_sg}"]
