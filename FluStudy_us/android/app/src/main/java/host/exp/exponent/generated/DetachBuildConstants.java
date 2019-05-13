47946e296d3a383b2c9f1dbc9b57b25e927e7741
Terri Paik <43700175+tpaik18@users.noreply.github.com>
2019-05-13 14:08:59 -0700

    Remove DetachBuildConstants from repo; generate as needed (#1069)

--- FluStudy_us/android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++
@@ -1,8 +0,0 @@
-package host.exp.exponent.generated;
-
-// This file is auto-generated. Please don't rename!
-public class DetachBuildConstants {
-
-  public static final String DEVELOPMENT_URL = "expdcd6f73fcd53486284683e2a09a2d545://10.165.69.63:19000";
-
-}
