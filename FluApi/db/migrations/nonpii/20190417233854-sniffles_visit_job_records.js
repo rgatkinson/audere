4280b56bddf0b18c276e683b42001003c3ae45b6
Michael Marucheck <mmarucheck@gmail.com>
2019-05-14 10:08:22 -0700

    Add copyright headers

--- FluApi/db/migrations/nonpii/20190417233854-sniffles_visit_job_records.js
+++ FluApi/db/migrations/nonpii/20190417233854-sniffles_visit_job_records.js
@@ -1,4 +1,9 @@
-'use strict';
+// Copyright (c) 2019 by Audere
+//
+// Use of this source code is governed by an MIT-style license that
+// can be found in the LICENSE file distributed with this file.
+
+"use strict";
 
 const {
   column,
