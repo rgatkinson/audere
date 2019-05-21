662532d3b105febb23e8b78e07cc3dd6899f9197
Terri Paik <43700175+tpaik18@users.noreply.github.com>
2019-05-21 10:49:40 -0700

    PLAT-47 Remove Firebase config from repo, CircleCI to inject during release build (#1099)
    * Remove Firebase config from repo, gitignore
    * Set prod vs staging Firebase config during build
    * Update publishedURL to FluAtHomeAU
    * Build script incrementing Cough build number to 6

--- FluStudy_au/android/app/src/main/java/host/exp/exponent/MainActivity.java
+++ FluStudy_au/android/app/src/main/java/host/exp/exponent/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends DetachActivity {
 
   @Override
   public String publishedUrl() {
-    return "exp://exp.host/@audere/FluAtHome";
+    return "exp://exp.host/@audere/FluAtHomeAU";
   }
 
   @Override
