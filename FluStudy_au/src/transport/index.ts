9f288a76a0845a9d77dbb47f96b9d581428d62e6
Michael Marucheck <mmarucheck@gmail.com>
2019-05-29 22:00:01 -0700

    PLAT-50 Remove PouchDB depenedency from Cough

--- FluStudy_au/src/transport/index.ts
+++ FluStudy_au/src/transport/index.ts
@@ -3,14 +3,10 @@
 // Use of this source code is governed by an MIT-style license that
 // can be found in the LICENSE file distributed with this file.
 
-import PouchDB from "pouchdb-react-native";
-import CryptoPouch from "crypto-pouch";
 import URL from "url-parse";
 import { Constants } from "expo";
 
 const IS_NODE_ENV_DEVELOPMENT = process.env.NODE_ENV === "development";
-
-PouchDB.plugin(CryptoPouch);
 
 export function getApiBaseUrl(): string {
   let api: string;
