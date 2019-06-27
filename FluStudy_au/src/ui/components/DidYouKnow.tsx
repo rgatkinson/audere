d4301da6a26af1ba30999b392a522a0d98b556bd
Zach Marucheck <zmarucheck@gmail.com>
2019-06-27 16:19:50 -0700

    Still WIP but getting there

--- FluStudy_au/src/ui/components/DidYouKnow.tsx
+++ FluStudy_au/src/ui/components/DidYouKnow.tsx
@@ -12,6 +12,9 @@ import { connect } from "react-redux";
 import { StoreState } from "../../store";
 import Text from "./Text";
 
+const TIPS = [ "tip0", "tip1", "tip2", "tip3", "tip4", "tip5", "tip6", "tip7", "tip8", "tip9", "tip10", "tip11", "tip12" ];
+const SOURCES = [ "source0", "source1", "source2", "source3", "source4", "source5", "source6", "source7", "source8", "source9", "source10", "source11", "source12", "source13" ];
+
 // This component will render up to this number of tips
 const TIP_COUNT = 8;
 // This acts as a multiplier for the source part of the text's size relative to the tip itself
@@ -40,7 +43,6 @@ class DidYouKnow extends React.Component<Props & WithNamespaces> {
   _willFocus: any;
 
   componentDidMount() {
-    const { t } = this.props;
     this._showNextTip();
     this._setTimer();
   }
@@ -63,8 +65,8 @@ class DidYouKnow extends React.Component<Props & WithNamespaces> {
     if (this.props.navigation.isFocused()) {
       const { t } = this.props;
       const currentTextNum = this._getCurrentTextNum();
-      const currentText = t("didYouKnow:tip" + currentTextNum);
-      const currentSource = t("didYouKnow:source" + currentTextNum);
+      const currentText = t("didYouKnow:" + TIPS[currentTextNum]);
+      const currentSource = t("didYouKnow:" + SOURCES[currentTextNum]);
       this.setState({ currentText, currentSource });
     }
   };
