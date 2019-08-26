9d701d545f04c405e651ae31d6fdf5798eb87f9a
Jaystar2323 <47769047+Jaystar2323@users.noreply.github.com>
2019-08-26 16:58:44 -0700

    FEV-558 Adding license headers (#98)
    * FEV-558 Adding license headers
    
    Takes in wildcards from the command line and can edit all those files recursively
    
    * Some code clean up
    
    * now actually edits the files

--- scripts/update_copyright.py
+++ scripts/update_copyright.py
@@ -3,6 +3,8 @@ import argparse
 import sys
 import re
 import datetime
+from glob import glob
+
 
 now = datetime.datetime.now()
 header_text = [" Copyright (c) " + str(now.year) + " by Audere", "", 
@@ -10,8 +12,11 @@ header_text = [" Copyright (c) " + str(now.year) + " by Audere", "",
 " can be found in the LICENSE file distributed with this file."]
 
 parser = argparse.ArgumentParser()
-parser.add_argument("inputfile")
-test_path = parser.parse_args().inputfile
+parser.add_argument("inputfilespec", nargs='+')
+parser.add_argument("-r", nargs=1, help="Enables it to search through directories recursivly, give path to starting directory")
+args = parser.parse_args()
+recursive = args.r
+wildcard_pattern = args.inputfilespec
 search_length = 5
 
 def editFile(path):
@@ -20,15 +25,18 @@ def editFile(path):
     try:
         text = f.readlines()
     except:
-        sys.stderr.write("Could not read file")
+        sys.stderr.write("Could not read file\n")
         return
     f.close()
+    if(len(text) == 0):
+        return
     header_found = False
     for i in range(search_length if len(text)>=search_length else len(text)):
-        old_str = re.search(r"\W+?Copyright\s+\(c\)\s+(?P<year1>\d{4})(?P<year2>\s*-\s*\d{4})? by Audere",
+        old_str = re.search(r"\W*Copyright\s+\(c\)\s+(?P<year1>\d{4})(?P<year2>\s*[-,]\s*\d{4})? by Audere",
             text[i])
         if old_str is None:
             continue
+        
         if str(now.year) in old_str.string:
             return
             
@@ -39,19 +47,44 @@ def editFile(path):
         header_found = True
         break
     if not header_found:
-        print("Create new header")
         if(text[0].startswith("#!")):
-            text.insert(1, header_text[0])
+            text.insert(1, createHeader(path))
         else:
-            text.insert(0, header_text[0])
+            text.insert(0, createHeader(path))
 
-    f = open("test.txt", "w")
+    f = open(path, "w")
     f.writelines(text)
+    print("Edited " + path)
     f.close()
 
-for (subdir, dirs, files) in os.walk(".."):
-    for file in files:
-        filepath = subdir + os.sep + file
-        #editFile(filepath)
-        #print(filepath)
-editFile(test_path)
+def createHeader(file):
+    header = ""
+    comment_type = "//"
+    is_block = False
+    if file.endswith(".py") or file.endswith(".sh"):
+        comment_type = "#"
+    if file.endswith(".css"):
+        comment_type = "*/"
+        is_block = True
+        header = "/* \n"
+    if file.endswith(".html"):
+        comment_type = "-->"
+        is_block = True
+        header = "<!-- \n"
+    for line in header_text:
+        header += (comment_type if not is_block else "") + line + "\n"
+    if is_block:
+        header += comment_type + "\n"
+    return header
+
+def processDirectory(path):
+    for pattern in wildcard_pattern:
+        for filename in glob(os.path.join(path, pattern)):
+            if not os.path.isdir(filename):
+                editFile(filename)
+
+if recursive is not None:
+    for (subdir, dirs, files) in os.walk(recursive[0]):
+        processDirectory(subdir)
+else:
+    processDirectory(".")
