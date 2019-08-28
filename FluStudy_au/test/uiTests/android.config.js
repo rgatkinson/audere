9836e1aeec47944179a871b03025b26301575e25
Sam Smedinghoff <smeds1@users.noreply.github.com>
2019-08-28 10:24:09 -0700

    Updated to appium beta and added real device capabilities (#127)

--- FluStudy_au/test/uiTests/android.config.js
+++ FluStudy_au/test/uiTests/android.config.js
@@ -6,17 +6,33 @@
 const os = require("os");
 const path = require("path");
 
+device_list = {
+  "Android Emulator": { X: 1435, Y: 2375, version: "9", realDevice: false },
+  "Sam's Galaxy": { X: 1080, Y: 1920, version: "7.0", realDevice: true },
+};
+
+const device = process.env.TEST_UI_DEVICE
+  ? process.env.TEST_UI_DEVICE
+  : "Android Emulator";
+
+if (!(device in device_list)) {
+  throw `${device} is not an accepted device`;
+}
+
+const apk = device_list[device].realDevice
+  ? "audere/FluStudy_au/android/app/build/outputs/apk/debug/app-armeabi-v7a-debug.apk"
+  : "audere/FluStudy_au/android/app/build/outputs/apk/debug/app-x86-debug.apk";
+
 const PLATFORM = "Android";
 const config = {
   platformName: "Android",
-  platformVersion: "9",
-  deviceName: "Android Emulator",
-  app: path.join(
-    os.homedir(),
-    "audere/FluStudy_au/android/app/build/outputs/apk/debug/app-x86-debug.apk"
-  ),
+  platformVersion: device_list[device].version,
+  deviceName: device,
+  app: path.join(os.homedir(), apk),
   automationName: "UiAutomator2",
+  appWaitForLaunch: false,
 };
-const SCREEN_X = 1435;
-const SCREEN_Y = 2375;
+
+const SCREEN_X = device_list[device].X;
+const SCREEN_Y = device_list[device].Y;
 module.exports = { PLATFORM, config, SCREEN_X, SCREEN_Y };
