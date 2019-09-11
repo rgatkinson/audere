d412235c598cb94049d2e86a1368da7f6d17062d
Terri Paik <43700175+tpaik18@users.noreply.github.com>
2019-09-11 12:54:48 -0700

    FEV-558 add copyright header (#178)

--- FluTrack/src/ExternalModules.d.ts
+++ FluTrack/src/ExternalModules.d.ts
@@ -1,3 +1,7 @@
+// Copyright (c) 2019 by Audere
+//
+// Use of this source code is governed by an MIT-style license that
+// can be found in the LICENSE file distributed with this file.
 // Declare external modules here so that TS noImplicitAny does not give errors
 
 declare module "redux-persist-transform-immutable";
