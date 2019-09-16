c2d87ba181f3f2882c207f665900245ab81a6414
goldjay <jaysteingold@gmail.com>
2019-09-16 14:32:09 -0700

    App support copy changed to be conditional

--- FluStudy_au/src/resources/MenuConfig.ts
+++ FluStudy_au/src/resources/MenuConfig.ts
@@ -14,6 +14,7 @@ import { PRIMARY_COLOR, LARGE_TEXT } from "../ui/styles";
 import { testSupport, appSupport } from "./LinkConfig";
 import Button from "../ui/components/Button";
 import { AppEvents } from "../util/tracker";
+import { getAppSupportText } from "../util/giftCard";
 
 function menuScreen(
   key: string,
@@ -116,6 +117,7 @@ export const MenuScreens: ScreenConfig[] = [
       {
         tag: ScreenText,
         props: {
+          conditionalTextFn: getAppSupportText,
           label: "appSupportDesc",
         },
       },
--- FluStudy_au/src/util/giftCard.ts
+++ FluStudy_au/src/util/giftCard.ts
@@ -10,6 +10,12 @@ export function getParticipantInfoText() {
   return getRemoteConfig("giftCardsAvailable") ? "desc2Incentive" : "desc2";
 }
 
+export function getAppSupportText() {
+  return getRemoteConfig("giftCardsAvailable")
+    ? "appSupportGiftCardsDesc"
+    : "appSupportDesc";
+}
+
 export async function getGiftCardAmount() {
   const state = (await getStore()).getState();
   if (state.meta.giftCardAmount === DEFAULT_GIFT_CARD_AMOUNT) {
