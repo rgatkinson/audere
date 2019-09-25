cd9a13912829b4acbac8d9e0391b95d8b5294d85
Philip Su <42978089+auderephilip@users.noreply.github.com>
2019-09-25 08:56:35 -0700

    Commit WhatsApp data into Firebase (#93)
    * Commit WhatsApp data into Firebase
    
    This introduces WhatsApp EVD data into Firebase using the same schema as the RN app.  We fake some things (e.g. GPS location), but can do future commits that update what we can from user data.
    
    * Removing needless debug log
    
    * Updated based on code review feedback

--- EbPhotoSite/functions/src/index.ts
+++ EbPhotoSite/functions/src/index.ts
@@ -5,10 +5,22 @@
 
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
-import { NotificationRequest } from "audere-lib/dist/ebPhotoStoreProtocol";
+import {
+  NotificationRequest,
+  PatientInfo,
+  HealthWorkerInfo,
+  EncounterInfo,
+  FirestoreProtocolDocument,
+  TransportMetadata,
+  DeviceInfo,
+} from "audere-lib/dist/ebPhotoStoreProtocol";
 import { SessionsClient } from "dialogflow";
 // @ts-ignore: Apparently types not yet available for dialogflow-fulfillment
 import { WebhookClient } from "dialogflow-fulfillment";
+import * as uuid from "uuid";
+import axios from "axios";
+import * as fs from "fs";
+import * as tmp from "tmp";
 
 admin.initializeApp();
 
@@ -68,9 +80,8 @@ export const onReceiveWhatsApp = functions.https.onRequest(
     let message = undefined;
 
     if (params.NumMedia == 1) {
-      console.log(
-        "Appears to be media",
-        params.MediaContentType0,
+      await onCompletePatientRecord(
+        getMobileFromSessionId(params.From),
         params.MediaUrl0
       );
       message =
@@ -273,27 +284,38 @@ function getCHWMobile(agent: WebhookAgent): string {
   const sessionParts = agent.session.split("/");
   const sessionId = sessionParts[sessionParts.length - 1];
 
+  return getMobileFromSessionId(sessionId);
+}
+
+function getMobileFromSessionId(sessionId: string) {
   if (sessionId.startsWith(WHATSAPP_PREFIX)) {
     return sessionId.substring(WHATSAPP_PREFIX.length);
   }
   return sessionId;
 }
 
+async function getCHWContextFromMobile(mobile: string) {
+  const chwSnap = await admin
+    .firestore()
+    .collection("whatsAppCHWs")
+    .doc(mobile)
+    .get();
+  if (chwSnap.exists) {
+    return chwSnap.data() as CHWContext;
+  }
+  return undefined;
+}
+
 async function getCHWGivenName(agent: WebhookAgent): Promise<string> {
   const chwContext = getFinalContextParams(agent, "chwwelcome-followup-yes") as
     | CHWContext
     | undefined;
-  if (chwContext) {
+  if (chwContext && chwContext.chwGivenName) {
     return chwContext.chwGivenName;
   }
-  const chwSnap = await admin
-    .firestore()
-    .collection("whatsAppCHWs")
-    .doc(getCHWMobile(agent))
-    .get();
-  if (chwSnap.exists) {
-    const chw = chwSnap.data() as CHWContext;
-    return chw.chwGivenName;
+  const chwFromFirebase = await getCHWContextFromMobile(getCHWMobile(agent));
+  if (chwFromFirebase) {
+    return chwFromFirebase.chwGivenName;
   }
   console.log(`Unexpectedly didn't find CHW record for ${getCHWMobile(agent)}`);
   return "Community Health Worker";
@@ -311,10 +333,11 @@ async function onNewPatient(agent: WebhookAgent) {
         .firestore()
         .collection("whatsAppPatients")
         .doc(patientContext.mobile as string)
-        .set(
-          { ...patientContext, chwMobile: getCHWMobile(agent) },
-          { merge: true }
-        ),
+        .set({
+          ...patientContext,
+          chwMobile: getCHWMobile(agent),
+          updatedAt: new Date().toISOString(),
+        }),
     ]);
     agent.add(
       `Thank you for registering ${patientContext.givenName}, ` +
@@ -333,8 +356,7 @@ async function onNewCHW(agent: WebhookAgent) {
     | undefined;
   if (chwContext) {
     const chw = {
-      givenName: chwContext.chwGivenName,
-      lastName: chwContext.chwLastName,
+      ...chwContext,
       mobile: getCHWMobile(agent),
     };
 
@@ -342,13 +364,187 @@ async function onNewCHW(agent: WebhookAgent) {
       .firestore()
       .collection("whatsAppCHWs")
       .doc(chw.mobile)
-      .set(chw, { merge: true });
+      .set(chw);
     agent.add(
       `Thank you for registering as a community health worker, ` +
-        `${chw.givenName}!  Whenever you'd like to register a new patient, ` +
+        `${
+          chw.chwGivenName
+        }!  Whenever you'd like to register a new patient, ` +
         `just say, "New patient."`
     );
   } else {
     console.log(`No CHW context in ${JSON.stringify(agent.context)}`);
+  }
+}
+
+async function getOrCreateUid(chwMobile: string) {
+  try {
+    const user = await admin.auth().getUserByPhoneNumber(chwMobile);
+    return user.uid;
+  } catch {
+    const chwRecord = await getCHWContextFromMobile(chwMobile);
+    const chwFullName = chwRecord
+      ? `${chwRecord.chwGivenName} ${chwRecord.chwLastName}`
+      : "unknown";
+    const user = await admin
+      .auth()
+      .createUser({ phoneNumber: chwMobile, displayName: chwFullName });
+
+    return user.uid;
+  }
+}
+
+async function getLatestPatient(
+  chwMobile: string
+): Promise<
+  { patient: PatientDemographics; totalPatients: number } | undefined
+> {
+  const snaps = await admin
+    .firestore()
+    .collection("whatsAppPatients")
+    .where("chwMobile", "==", chwMobile)
+    .orderBy("updatedAt", "desc")
+    .get();
+
+  if (snaps.empty) {
+    console.log(`Couldn't find latest patient for CHW ${chwMobile}`);
+    return undefined;
+  }
+
+  return {
+    patient: snaps.docs[0].data() as PatientDemographics,
+    totalPatients: snaps.docs.length,
+  };
+}
+
+async function createEncounterDoc(
+  docId: string,
+  chwMobile: string,
+  photoId: string
+): Promise<FirestoreProtocolDocument | undefined> {
+  const timestamp = new Date().toISOString();
+  const _transport: TransportMetadata = {
+    lastWriter: "sender",
+    protocolVersion: 1,
+    sentAt: timestamp,
+  };
+  const device: DeviceInfo = {
+    clientBuild: "10001",
+    clientVersion: {
+      buildDate: "20190816",
+      hash: "randomhashstringforcloudfunction",
+      name: "EbPhotoSiteCloudFunction",
+      version: "1.0",
+    },
+    idiomText: "WhatsApp",
+    installation: chwMobile,
+    platform: {
+      osName: "unknown",
+      osVersion: "unknown",
+    },
+  };
+  const [uid, chwContext, patientData] = await Promise.all([
+    getOrCreateUid(chwMobile),
+    getCHWContextFromMobile(chwMobile),
+    getLatestPatient(chwMobile),
+  ]);
+
+  if (!chwContext || !patientData) {
+    console.log(`Failed to get CHW ${chwMobile} or latest patient.`);
+    return undefined;
+  }
+  const patientDemographics = patientData.patient;
+  const patient: PatientInfo = {
+    firstName: patientDemographics.givenName,
+    lastName: patientDemographics.lastName,
+    phone: patientDemographics.mobile,
+  };
+  const healthWorker: HealthWorkerInfo = {
+    firstName: chwContext.chwGivenName,
+    lastName: chwContext.chwLastName,
+    notes: "",
+    phone: chwMobile,
+    uid,
+  };
+  const encounter: EncounterInfo = {
+    healthWorker,
+    isDemo: false,
+    localIndex: (patientData.totalPatients - 1).toString(),
+    notes: "",
+    patient,
+    rdtPhotos: [
+      {
+        gps: getPhotoGPS(),
+        photoId,
+        timestamp,
+      },
+    ],
+    updatedAt: timestamp,
+  };
+  const doc: FirestoreProtocolDocument = {
+    _transport,
+    device,
+    docId,
+    // @ts-ignore DocumentType is declared an enum.  When deployed to Firebase
+    // Cloud Functions, the server side can't find these enums.
+    documentType: "ENCOUNTER",
+    encounter,
+    schemaId: 1,
+  };
+  return doc;
+}
+
+// Lame for now, will soon extract from EXIF.  Hardcoded to Bay Area right now.
+function getPhotoGPS() {
+  return {
+    latitude: "37.4219983",
+    longitude: "-122.084",
+  };
+}
+
+async function downloadUri(uri: string) {
+  const response = await axios.request({
+    responseType: "arraybuffer",
+    url: uri,
+    method: "get",
+    headers: {
+      "Content-Type": "image/jpeg",
+    },
+  });
+  const localFile = tmp.tmpNameSync();
+  fs.writeFileSync(localFile, response.data);
+  return localFile;
+}
+
+async function onCompletePatientRecord(chwMobile: string, photoUri: string) {
+  const photoId = uuid();
+  let localFile = null;
+
+  try {
+    localFile = await downloadUri(photoUri);
+    await admin
+      .storage()
+      .bucket()
+      .upload(localFile, {
+        destination: `photos/${photoId}.jpg`,
+        contentType: "image/jpeg",
+      });
+  } finally {
+    if (localFile) {
+      fs.unlinkSync(localFile);
+    }
+  }
+
+  const docId = uuid();
+  const encounter = await createEncounterDoc(docId, chwMobile, photoId);
+
+  if (encounter) {
+    await admin
+      .firestore()
+      .collection("encounters")
+      .doc(docId)
+      .set(encounter);
+  } else {
+    console.log(`Could not create an encounter for ${chwMobile}`);
   }
 }
