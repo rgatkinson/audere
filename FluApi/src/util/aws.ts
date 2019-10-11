3ccbfa129a59af3f17b5375862cc889f712f8056
billy-audere <45672302+billy-audere@users.noreply.github.com>
2019-10-11 10:36:15 -0700

    Updating AWS provider chain to use ECS (#290)

--- FluApi/src/util/aws.ts
+++ FluApi/src/util/aws.ts
@@ -9,7 +9,7 @@ AWS.config.update({
   region: "us-west-2",
   credentialProvider: new AWS.CredentialProviderChain([
     () => new AWS.SharedIniFileCredentials(),
-    () => new AWS.EC2MetadataCredentials(),
+    () => new AWS.ECSCredentials(),
   ]),
 });
 
