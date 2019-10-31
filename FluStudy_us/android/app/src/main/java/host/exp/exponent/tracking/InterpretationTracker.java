1f96470a78ada6394b2b20ef18f9cce1b391bedd
Jenny Abrahamson <jenny.abrahamson@gmail.com>
2019-10-31 15:50:03 -0700

    Take high res RDT image when filters pass (#336)

--- FluStudy_us/android/app/src/main/java/host/exp/exponent/tracking/InterpretationTracker.java
+++ FluStudy_us/android/app/src/main/java/host/exp/exponent/tracking/InterpretationTracker.java
@@ -5,8 +5,6 @@
 
 package host.exp.exponent.tracking;
 
-import android.app.Activity;
-import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.RectF;
 import android.util.Log;
@@ -15,45 +13,13 @@ import android.util.Pair;
 import host.exp.exponent.DetectorView;
 import host.exp.exponent.tflite.Classifier;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-public class InterpretationTracker extends Tracker {
-    private static int FRAMES_TO_INTERPRET = 4;
+public class InterpretationTracker {
     private static final String TAG = "InterpretationTracker";
 
-    private final List<List<RDTTracker.TrackedRecognition>> allTrackedObjects = new ArrayList<>();
-
-    public InterpretationTracker(final Activity activity) {
-        super(activity);
-    }
-
-    private boolean sentResults;
-
-    /*
-    @Override
-    public synchronized void draw(final Canvas canvas, boolean demoMode) {
-        /*if (demoMode && allTrackedObjects.size() > 0) {
-            for (final RDTTracker.TrackedRecognition recognition : allTrackedObjects.get(allTrackedObjects.size() - 1)) {
-                final RectF trackedPos = new RectF(recognition.location);
-
-                boxPaint.setColor(recognition.color);
-
-                float cornerSize = Math.min(trackedPos.width(), trackedPos.height()) / 8.0f;
-                float leftOffset = canvas.getWidth() - (TF_OD_API_INPUT_SIZE / 2) - TEST_AREA_INSET_MARGIN;
-                float topOffset = canvas.getHeight() - (TF_OD_API_INPUT_SIZE / 2) - TEST_AREA_INSET_MARGIN;
-                canvas.drawRoundRect(leftOffset + trackedPos.left, topOffset + trackedPos.top, leftOffset + trackedPos.right, topOffset + trackedPos.bottom, cornerSize, cornerSize, boxPaint);
-
-                final String labelString = getLabel(recognition);
-                borderedText.drawText(canvas, leftOffset - TF_OD_API_INPUT_SIZE, topOffset + trackedPos.top, labelString + "%", boxPaint);
-            }
-        }
-    }
-    */
-
-    @Override
-    public synchronized void trackResults(final List<Classifier.Recognition> results) {
+    public static synchronized DetectorView.InterpretationResult interpretResults(final List<Classifier.Recognition> results) {
         Log.i(TAG, "tracking interpretation result");
         final List<Pair<Float, Classifier.Recognition>> rectsToTrack = new LinkedList<Pair<Float, Classifier.Recognition>>();
 
@@ -66,7 +32,7 @@ public class InterpretationTracker extends Tracker {
         }
         List<RDTTracker.TrackedRecognition> trackedObjects = new LinkedList<>();
         if (rectsToTrack.isEmpty()) {
-            Log.i(TAG, "Nothing to track, aborting.");
+            Log.i(TAG, "Nothing to track.");
         } else {
             for (final Pair<Float, Classifier.Recognition> potential : rectsToTrack) {
                 final RDTTracker.TrackedRecognition trackedRecognition = new RDTTracker.TrackedRecognition();
@@ -77,59 +43,33 @@ public class InterpretationTracker extends Tracker {
                 trackedObjects.add(trackedRecognition);
             }
         }
-        allTrackedObjects.add(trackedObjects);
 
         Log.d(TAG, "All results");
-        Log.d(TAG, "\n" + getAllResultsForDisplay());
+        Log.d(TAG, "\n" + getAllResultsForDisplay(trackedObjects));
+
+        return getInterpretationResult(trackedObjects);
     }
 
-    private String getAllResultsForDisplay() {
-        StringBuilder allResults = new StringBuilder();
-        for (int i = 0; i < allTrackedObjects.size(); i++) {
-            List<RDTTracker.TrackedRecognition> trackedObjects = allTrackedObjects.get(i);
-            allResults.append("\n" + i);
-            for (RDTTracker.TrackedRecognition recognition : trackedObjects) {
-                allResults.append("   " + getLabel(recognition) + "\n");
-            }
+    private static String getAllResultsForDisplay(List<RDTTracker.TrackedRecognition> trackedObjects) {
+        StringBuilder allResults = new StringBuilder("\nResults:\n");
+        for (RDTTracker.TrackedRecognition recognition : trackedObjects) {
+            allResults.append("   " + Tracker.getLabel(recognition) + "\n");
         }
         return allResults.toString();
     }
 
-    public boolean shouldSendResults() {
-        if (!sentResults && allTrackedObjects.size() >= FRAMES_TO_INTERPRET) {
-            sentResults = true;
-            return true;
-        }
-        return false;
-    }
-
-    public DetectorView.InterpretationResult getInterpretationResult() {
+    public static DetectorView.InterpretationResult getInterpretationResult(List<RDTTracker.TrackedRecognition> trackedObjects) {
         DetectorView.InterpretationResult interpretationResult = new DetectorView.InterpretationResult();
 
-        interpretationResult.samples = allTrackedObjects.size();
-        interpretationResult.requiredSamples = FRAMES_TO_INTERPRET;
-
-        // First pass attempt at combining results:
-        // Return control | testA | testB if at least half the samples showed that result
-        int controlCount = 0;
-        int aCount = 0;
-        int bCount = 0;
-
-        for (List<RDTTracker.TrackedRecognition> trackedObjects : allTrackedObjects) {
-            for (RDTTracker.TrackedRecognition recognition : trackedObjects) {
-                if (recognition.title.equals("control")) {
-                    controlCount++;
-                } else if (recognition.title.equals("flu-a")) {
-                    aCount++;
-                } else if (recognition.title.equals("flu-b")) {
-                    bCount++;
-                }
+        for (RDTTracker.TrackedRecognition recognition : trackedObjects) {
+            if (recognition.title.equals("control")) {
+                interpretationResult.control = true;
+            } else if (recognition.title.equals("flu-a")) {
+                interpretationResult.testA = true;
+            } else if (recognition.title.equals("flu-b")) {
+                interpretationResult.testB = true;
             }
         }
-
-        interpretationResult.control = controlCount >= allTrackedObjects.size() / 2;
-        interpretationResult.testA = aCount >= allTrackedObjects.size() / 2;
-        interpretationResult.testB = bCount >= allTrackedObjects.size() / 2;
 
         return interpretationResult;
     }
--- FluStudy_us/android/app/src/main/java/host/exp/exponent/tracking/Tracker.java
+++ FluStudy_us/android/app/src/main/java/host/exp/exponent/tracking/Tracker.java
@@ -6,62 +6,27 @@
 package host.exp.exponent.tracking;
 
 import android.app.Activity;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.graphics.RectF;
 import android.text.TextUtils;
-import android.util.DisplayMetrics;
-import android.util.TypedValue;
-
-import host.exp.exponent.env.BorderedText;
-import host.exp.exponent.tflite.Classifier;
-
-import java.util.List;
 
 public abstract class Tracker {
-    private static final float TEXT_SIZE_DIP = 18;
     protected static final int TF_OD_API_INPUT_SIZE = 300;
-
-    protected final Paint boxPaint = new Paint();
-    protected final float textSizePx;
-    protected final BorderedText borderedText;
 
     protected int previewWidth;
     protected int previewHeight;
     protected int sensorOrientation;
-
-    protected final int TEST_AREA_INSET_MARGIN = 200;
-
     protected int screenHeight;
     protected int screenWidth;
 
-    public Tracker(final Activity activity) {
-        boxPaint.setColor(Color.RED);
-        boxPaint.setStyle(Paint.Style.STROKE);
-        boxPaint.setStrokeWidth(10.0f);
-        boxPaint.setStrokeCap(Paint.Cap.ROUND);
-        boxPaint.setStrokeJoin(Paint.Join.ROUND);
-        boxPaint.setStrokeMiter(100);
-
-        textSizePx = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, TEXT_SIZE_DIP,
-                activity.getResources().getDisplayMetrics());
-        borderedText = new BorderedText(textSizePx);
-    }
-
-    public synchronized void setPreviewConfiguration(final int width, final int height, final int sensorOrientation, final int screenWidth, final int screenHeight) {
-        previewWidth = width;
-        previewHeight = height;
+    public Tracker(final Activity activity, int previewWidth, int previewHeight, final int sensorOrientation, final int screenWidth, final int screenHeight) {
+        this.previewWidth = previewWidth;
+        this.previewHeight = previewHeight;
         this.sensorOrientation = sensorOrientation;
         this.screenWidth = screenWidth;
         this.screenHeight = screenHeight;
     }
 
-    public abstract void trackResults(final List<Classifier.Recognition> results);
-
-    // public abstract void draw(final Canvas canvas, boolean demoMode);
-
-    protected String getLabel(RDTTracker.TrackedRecognition recognition) {
+    public static String getLabel(RDTTracker.TrackedRecognition recognition) {
         return !TextUtils.isEmpty(recognition.title)
                 ? String.format("%s %.2f", recognition.title, (100 * recognition.detectionConfidence))
                 : String.format("%.2f", (100 * recognition.detectionConfidence));
