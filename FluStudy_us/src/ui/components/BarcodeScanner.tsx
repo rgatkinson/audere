ec40a366550a231d7555e1c6e5a0a71828855e1b
Rob <rob@auderenow.org>
2019-11-25 17:09:17 -0800

    Switch barcode scanner over to using camera2 API

--- FluStudy_us/src/ui/components/BarcodeScanner.tsx
+++ FluStudy_us/src/ui/components/BarcodeScanner.tsx
@@ -187,6 +187,7 @@ class BarcodeScanner extends React.Component<Props & WithNamespaces> {
         <Camera
           style={StyleSheet.absoluteFill}
           onBarCodeScanned={this._onBarCodeScanned}
+          useCamera2Api={true}
         />
         <View style={styles.overlayContainer}>
           <View style={styles.targetBox} />
